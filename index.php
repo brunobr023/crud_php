be18fce95dfae6e80f8ef0542f1d19babccfe7fb
Ramiro <152666157+GuRamiroo27@users.noreply.github.com>
2024-04-29 20:33:31 -0300

    FormularioPhp

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <form  method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
-	<label for="nota1">Nota 1:</label>
+	<label for="Nome">Nota 1:</label>
 	<input type="number" name="nota1" required><br>
 	
-	<label for="nota2">Nota 2:</label>
+	<label for="Telefone">Nota 2:</label>
 	<input type="number" name="nota2" required><br>
 	
-	<label for="nota3">Nota 3:</label>
+	<label for="">Nota 3:</label>
 	<input type="number" name="nota3" required><br>
 	
 	<label for="nota4">Nota 4:</label>
