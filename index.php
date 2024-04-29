af7945371f577adbac970846330b41386421de24
Bruno Otavio <100975408+brunobr023@users.noreply.github.com>
2024-04-29 20:16:19 -0300

    php bd

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
-$servername = "nome do servidor"
+$servername = "localhost";
+$username = "username";
+$password = "root";
+$dbname = "clientes";
 
-
-
-
-
-
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // Define o modo de erro do PDO para exceção
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Conexão realizada com sucesso!";
+} catch(PDOException $e) {
+  echo "Conexão falhou: " . $e->getMessage();
+}
 
 
 ?>
