b9ff3577949b36e25df77330c339f20623da668e
brunobr023 <brunootavio326@gmail.com>
2024-04-30 20:27:12 -0300

    ATUALIZAÇÃO 0.0.1

--- index.php
+++ index.php
@@ -1,25 +1,24 @@
 <?php
+	include dbcontecion.php;
+	include header.php;
+
 	require_once("dbconection.php");
 	$con = new Database();
 	$link = $con->getConexao();
-?>
 
-<form  method="post" action="dbcontecion.php">
-	<label for="Nome">Nota 1:</label>
-	<input type="text" name="nome_Cliente" required><br>
+	<form  method="post" action="dbcontecion.php">
+		<label for="Nome">Nota 1:</label>
+		<input type="text" name="nome_Cliente" required><br>
 	
-	<label for="Telefone">Nota 2:</label>
-	<input type="number" name="telefone_Cliente" required><br>
+		<label for="Telefone">Nota 2:</label>
+		<input type="number" name="telefone_Cliente" required><br>
 	
-	<label for="Email">Nota 3:</label>
-	<input type="email" name="email_Cliente" required><br>
+		<label for="Email">Nota 3:</label>
+		<input type="email" name="email_Cliente" required><br>
 	
-	<label for="Compra">Nota 4:</label>
-	<input type="text" name="compra_Cliente" required><br>
+		<label for="Compra">Nota 4:</label>
+		<input type="text" name="compra_Cliente" required><br>
 	
-	<input type="submit" value="Enviar Dados">
+		<input type="submit" value="Enviar Dados">
 </form>
-<?php
-	
-
 ?>
